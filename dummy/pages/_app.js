53d6216bcfa033779485d1309ef530231dfafe71
goncaloarnauth <goncaloarnauth@outlook.com>
2022-09-05 12:48:54 +0100

    remove header script

--- dummy/pages/_app.js
+++ dummy/pages/_app.js
@@ -7,21 +7,21 @@ import Head from "next/head";
 const MyCookie = () => {
     const domainGroupId = "4c7db490-c6f2-480a-93f5-5bd82fcad3ce"
 console.log("here !!")
-    return <script id="CookieDeclaration" src="https://consent.cookiebot.com/4c7db490-c6f2-480a-93f5-5bd82fcad3ce/cd.js" type="text/javascript" async></script>
+    return (<CookieBot domainGroupId={domainGroupId} />)
 }
 
 function MyApp({ Component, pageProps }) {
   return (
       <>
           <Head>
-              <script
-                  id="Cookiebot"
-                  src="https://consent.cookiebot.com/uc.js"
-                  data-cbid="4c7db490-c6f2-480a-93f5-5bd82fcad3ce"
-                  data-blockingmode="auto"
-                  type="text/javascript"
-                  async
-              />
+              {/*<script*/}
+              {/*    id="Cookiebot"*/}
+              {/*    src="https://consent.cookiebot.com/uc.js"*/}
+              {/*    data-cbid="4c7db490-c6f2-480a-93f5-5bd82fcad3ce"*/}
+              {/*    data-blockingmode="auto"*/}
+              {/*    type="text/javascript"*/}
+              {/*    async*/}
+              {/*/>*/}
               <title>Dummy test</title>
           </Head>
           <ContentLayout>
