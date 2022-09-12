c5b9537619144af078433bffcfd4dbf6b5e81655
goncaloarnauth <goncaloarnauth@outlook.com>
2022-09-12 11:39:53 +0100

    add data layer push

--- dummy/pages/index.js
+++ dummy/pages/index.js
@@ -1,4 +1,17 @@
+import {useEffect, useState} from "react";
+
 const Home = () => {
+
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    useEffect(() => {
+        window.dataLayer.push({
+            event: 'isAdmin event',
+            eventProps: {
+                isAdmin: isAdmin,
+            }
+        });
+    }, [isAdmin])
 
   return (
       <div>
@@ -8,6 +21,9 @@ const Home = () => {
           <div className="userTable">
              Dummy content
           </div>
+          <button onClick={() => setIsAdmin(!isAdmin)}>
+              {isAdmin ? "I'm a Admin" : "I'm a normal user"}
+          </button>
       </div>
   )
 }
